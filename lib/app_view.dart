e1e7b077919116cf8612bc3fa758b45f4eff7ad1
vinidosreis <vinidosreis13@gmail.com>
2024-03-08 07:23:42 -0300

    nw signIn screen

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_app/blocs/authentication_bloc/authentication_bloc.dart';
+
+import 'screens/auth/views/welcome_view.dart';
+import 'screens/home/views/home_screen.dart';
 
 class MyAppView extends StatelessWidget {
+  
   const MyAppView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Pizza Delivery',
-    );
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          background: Colors.grey,
+          onBackground: Colors.black,
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+          )
+        ),
+        home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+          builder: ((context, state) {
+            if (state.status == AuthenticationStatus.authenticated) {
+              return const HomeScreen();
+            } else {
+              return const WelcomeScreen();
+            }
+          })
+        ),
+      );
   }
 }
