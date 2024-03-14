de9f90685a86cd5e111472479b9eb6486d15b584
vinidosreis <vinidosreis13@gmail.com>
2024-03-14 07:04:46 -0300

    start pizza repo

--- lib/screens/home/views/details_screen.dart
+++ lib/screens/home/views/details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../../../components/macro.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
@@ -6,7 +8,137 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.background),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width - 40,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(3, 3),
+                      blurRadius: 5,
+                    )
+                  ],
+                  image:
+                      const DecorationImage(image: AssetImage("assets/1.png"))),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(3, 3),
+                      blurRadius: 5,
+                    )
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Expanded(
+                          flex: 2,
+                          child: Text("Truffle Temptation Extravaganza",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold)),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text("\$ 12.00",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary)),
+                                const Text("\$ 16.00",
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey,
+                                        decoration:
+                                            TextDecoration.lineThrough)),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 12),
+                    const Row(
+                      children: [
+                        MyMacroWidget(
+                          title: 'Calories',
+                          value: 879,
+                          icon: FontAwesomeIcons.fire,
+                        ),
+                        SizedBox(width: 10),
+                        MyMacroWidget(
+                          title: 'Protein',
+                          value: 36,
+                          icon: FontAwesomeIcons.dumbbell,
+                        ),
+                        SizedBox(width: 10),
+                        MyMacroWidget(
+                          title: 'Fat',
+                          value: 21,
+                          icon: FontAwesomeIcons.bowlFood,
+                        ),
+                        SizedBox(width: 10),
+                        MyMacroWidget(
+                          title: 'Carbs',
+                          value: 38,
+                          icon: FontAwesomeIcons.breadSlice,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 30),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: 50,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                            elevation: 3.0,
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        onPressed: () {},
+                        child: const Text(
+                        'Buy Now',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600
+                        )
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pizza_app/app.dart';
 import 'package:pizza_app/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
 import 'package:pizza_app/screens/home/views/details_screen.dart';
 
@@ -25,42 +24,39 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
         actions: [
-          IconButton(onPressed: () { 
-          }, 
-          icon: const Icon(CupertinoIcons.cart)
-          ),
-          IconButton(onPressed: () {
-            context.read<SignInBloc>().add(SignOutRequired());
-          }, 
-          icon: const Icon(CupertinoIcons.arrow_right_to_line)
-          )
+          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.cart)),
+          IconButton(
+              onPressed: () {
+                context.read<SignInBloc>().add(SignOutRequired());
+              },
+              icon: const Icon(CupertinoIcons.arrow_right_to_line))
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 16,
-            mainAxisSpacing: 16,
-            childAspectRatio: 9/16
-            ),
-          itemCount: 8, 
-          itemBuilder: (context, int i) {
-            return Material(
-              elevation: 3,
-              color: Colors.white,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-              child: InkWell(
-                borderRadius: BorderRadius.circular(20),
-                onTap: () {
-                  Navigator.push(
-                    context, 
-                    MaterialPageRoute<void>(
-                      builder: (BuildContext context) => const DetailsScreen(),
-                      )
-                    );
-                },
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: 9 / 16),
+            itemCount: 8,
+            itemBuilder: (context, int i) {
+              return Material(
+                elevation: 3,
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(20),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute<void>(
+                          builder: (BuildContext context) =>
+                              const DetailsScreen(),
+                        ));
+                  },
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -71,36 +67,34 @@ class HomeScreen extends StatelessWidget {
                           children: [
                             Container(
                               decoration: BoxDecoration(
-                                color: Colors.green,
-                                borderRadius: BorderRadius.circular(30)
-                              ),
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(30)),
                               child: const Padding(
-                                padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 4, horizontal: 8),
                                 child: Text(
                                   'NON-VEG',
                                   style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 8
-                                  ),
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 8),
                                 ),
                               ),
                             ),
                             const SizedBox(width: 8),
                             Container(
                               decoration: BoxDecoration(
-                                color: Colors.green.withOpacity(0.2),
-                                borderRadius: BorderRadius.circular(30)
-                              ),
+                                  color: Colors.green.withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(30)),
                               child: const Padding(
-                                padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 4, horizontal: 8),
                                 child: Text(
                                   '🌶️ Balance',
                                   style: TextStyle(
-                                    color: Colors.green,
-                                    fontWeight: FontWeight.w800,
-                                    fontSize: 8
-                                  ),
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 8),
                                 ),
                               ),
                             )
@@ -109,65 +103,61 @@ class HomeScreen extends StatelessWidget {
                       ),
                       const SizedBox(height: 8),
                       const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 12),
-                        child: Text(
-                          'Cheesy Marvel',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold
-                          ),
-                        )
-                        ),
-                         Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12),
-                        child: Text(
-                          'Your pizza, your rules, we have the best taste!',
-                          style: TextStyle(
-                            color: Colors.grey.shade700,
-                            fontSize: 10,
-                          ),
-                        )
-                        ),
-                        Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                              'R\$ 12',
-                              style: TextStyle(
-                                color: Theme.of(context).colorScheme.primary,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700
+                          padding: EdgeInsets.symmetric(horizontal: 12),
+                          child: Text(
+                            'Cheesy Marvel',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          )),
+                      Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          child: Text(
+                            'Your pizza, your rules, we have the best taste!',
+                            style: TextStyle(
+                              color: Colors.grey.shade700,
+                              fontSize: 10,
+                            ),
+                          )),
+                      Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    'R\$ 12',
+                                    style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    'R\$ 16',
+                                    style: TextStyle(
+                                        color: Colors.grey.shade500,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w700,
+                                        decoration: TextDecoration.lineThrough),
+                                  ),
+                                ],
                               ),
-                            ),
-                            const SizedBox(width: 5,),
-                              Text(
-                              'R\$ 16',
-                              style: TextStyle(
-                                color: Colors.grey.shade500,
-                                fontSize: 12,
-                                fontWeight: FontWeight.w700,
-                                decoration: TextDecoration.lineThrough
-                              ),
-                            ),
-                              ],
-                            ),
-                            IconButton(
-                              onPressed: () {}, 
-                              icon: const Icon(CupertinoIcons.add_circled_solid)
-                              )
-                          ],
-                        )
-                        )
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                      CupertinoIcons.add_circled_solid))
+                            ],
+                          ))
                     ],
                   ),
                 ),
               );
-          }
-          ),
+            }),
       ),
     );
   }
